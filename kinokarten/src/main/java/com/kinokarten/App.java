5643dda569fe745b2628d5792d0dec8ffb40f677
rollerhawk <nikitapuke23@gmail.com>
2024-02-21 20:07:16 +0100

    main

--- kinokarten/src/main/java/com/kinokarten/App.java
+++ kinokarten/src/main/java/com/kinokarten/App.java
@@ -1,5 +1,8 @@
 package com.kinokarten;
 
+import java.sql.Date;
+import java.time.LocalDateTime;
+
 /**
  * Hello world!
  *
@@ -9,6 +12,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        Termin _meinTermin;
+        _meinTermin = new Termin();
+        _meinTermin.set_startUhrzeit(LocalDateTime.now());
 
         Saal _meinSaal;
         Saal _meinSaal2;
---
+++ kinokarten/src/main/java/com/kinokarten/Film.java
@@ -0,0 +1,12 @@
+package com.kinokarten;
+
+public class Film {
+
+    private Termin _termin;
+    private String _titel;
+    private String _daurer;
+    private FSK _altersBeschraenkung;
+    public  Termin get_Termin() {
+        return _termin;
+    }
+}
