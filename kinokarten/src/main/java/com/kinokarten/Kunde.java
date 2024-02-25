c9f2101daba997cf32f8d08c6b888cda991da146
Felix Prenzel <felix.prenzel@gmx.de>
2024-02-25 13:32:40 +0100

    Kunde

---
+++ kinokarten/src/main/java/com/kinokarten/Kunde.java
@@ -0,0 +1,13 @@
+package com.kinokarten;
+
+public class Kunde {
+    private int _kundenNummer;
+    private Adresse _adresse;
+    private int _alter;
+    private String _vorname;
+    private String _nachname;
+
+    public Reservierung get_Reservierung(){
+        return _reservierung;
+    }
+}
