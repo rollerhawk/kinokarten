e885df6751f05c2fb3bb9af6373baeda83909210
rollerhawk <nikitapuke23@gmail.com>
2024-02-25 13:49:52 +0100

    Reservierung

--- kinokarten/src/main/java/com/kinokarten/Kunde.java
+++ kinokarten/src/main/java/com/kinokarten/Kunde.java
@@ -6,6 +6,7 @@ public class Kunde {
     private int _alter;
     private String _vorname;
     private String _nachname;
+    private Reservierung _reservierung;
 
     public Reservierung get_Reservierung(){
         return _reservierung;
--- kinokarten/src/main/java/com/kinokarten/Reservierung.java
+++ kinokarten/src/main/java/com/kinokarten/Reservierung.java
@@ -1,9 +1,17 @@
 package com.kinokarten;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Reservierung {
+    private long _reservierungsNr;
+    private Kunde _kunde;
     private List<Sitzplatz> _sitzplaetze;
 
+    public Reservierung(long _reservierungsNr, Kunde _kunde) {
+        this._reservierungsNr = _reservierungsNr;
+        this._kunde = _kunde;
+        this._sitzplaetze = new ArrayList<Sitzplatz>();
+    }
     
 }
