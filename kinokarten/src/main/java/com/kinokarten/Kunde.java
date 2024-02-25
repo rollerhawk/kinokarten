ff286af8d9b8180fe855f3d2019b764e356c996e
Felix Prenzel <felix.prenzel@gmx.de>
2024-02-25 13:54:13 +0100

    test

--- kinokarten/src/main/java/com/kinokarten/Kunde.java
+++ kinokarten/src/main/java/com/kinokarten/Kunde.java
@@ -1,6 +1,5 @@
 package com.kinokarten;
 
-<<<<<<< HEAD
 
     public class Kunde {
         private int _kundenNummer;
@@ -13,18 +12,3 @@ package com.kinokarten;
             return _reservierung;
         }
     }
-
-=======
-public class Kunde {
-    private int _kundenNummer;
-    private Adresse _adresse;
-    private int _alter;
-    private String _vorname;
-    private String _nachname;
-    private Reservierung _reservierung;
-
-    public Reservierung get_Reservierung(){
-        return _reservierung;
-    }
-}
->>>>>>> e885df6751f05c2fb3bb9af6373baeda83909210
