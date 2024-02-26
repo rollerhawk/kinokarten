3403cbb42428131d8236bc8eebb7f0e1c5f66243
Felix Prenzel <felix.prenzel@gmx.de>
2024-02-26 19:13:32 +0100

    manager

--- kinokarten/src/main/java/com/kinokarten/Interfaces/BuildInterface.java
+++ kinokarten/src/main/java/com/kinokarten/Interfaces/BuildInterface.java
@@ -1,5 +1,7 @@
 package com.kinokarten.Interfaces;
-
+/**
+ * Erzeugung des Interfaces für den Build
+ */
 public interface BuildInterface {
     void Build();
 }
--- kinokarten/src/main/java/com/kinokarten/Interfaces/CounterInterface.java
+++ kinokarten/src/main/java/com/kinokarten/Interfaces/CounterInterface.java
@@ -1,5 +1,7 @@
 package com.kinokarten.Interfaces;
-
+/**
+ * Erzeugung des 
+ */
 public interface CounterInterface {
     long GetNextNr();
 }
